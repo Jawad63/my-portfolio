40f05ed3cd86ee8c86d897fe315b2212b4fc29f8
Jawad63 <jawadqureshi111@gmail.com>
2021-09-21 15:31:14 +0200

    hamburger icon made

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,16 +1,33 @@
 import React from "react"
 import "./topbar.css"
+import {Person, Email} from "@material-ui/icons"
 
 export default function Topbar() {
    return (
       <div className="topbar">
          <div className="wrapper">
+
             <div className="left">
                <a href="#contact" className="logo">MY-PORTFOLIO</a>
-               
+
+               <div className="itemContainer">
+                  <Person className="icon"/>
+                  <span>+32 465 28 89 57</span>
+               </div>
+
+               <div className="itemContainer">
+                  <Email className="icon"/>
+                  <span>jawadqureshi111@gmail.com</span>
+               </div>
             </div>
-            <h1>Hello</h1>
-            <div className="right">This is right</div>
+
+            <div className="right">
+               <div className="hamburger">
+                  <span className="line1"></span>
+                  <span className="line2"></span>
+                  <span className="line3"></span>
+               </div>
+            </div>
          </div>
       </div>
    )
