b7c2ef6ebecf76bb3626f0214c9cee01f71c9590
Jawad63 <jawadqureshi111@gmail.com>
2021-09-22 10:43:16 +0200

    intro complete moving to portfolio section

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import "./intro.css"
-import {ArrowDownward} from "@material-ui/icons"
+import { ArrowDownward } from "@material-ui/icons"
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
 
 
 export default function Intro() {
+
+   const textRef = useRef();
+   useEffect(() => {
+      init(textRef.current, {
+         showCursor: true,
+         backDelay: 1500,
+         backSpeed: 60,
+         strings: ['Full stack Web-Developer'],
+      });
+   },[])
+
    return (
       <div className="intro" id="intro">
 
@@ -17,7 +30,7 @@ export default function Intro() {
             <div className="wrapper">
                <h2>Hello, I'm</h2>
                <h1>Jawad Qureshi</h1>
-               <h3>Full Stack <span>Web-Developer</span></h3>
+               <h3>I'm a <span ref={textRef}></span></h3>
             </div>
 
             <a href="#portfolio">
