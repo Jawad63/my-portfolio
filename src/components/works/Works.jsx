778394ebfe61f70e7c2991ed860e54d0f011bad5
Jawad63 <jawadqureshi111@gmail.com>
2021-09-24 15:54:26 +0200

    pushing again

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -4,7 +4,31 @@ import "./works.css"
 export default function Works() {
    return (
       <div className="works" id="works">
-         <h1>Works</h1>
+         <div className="left">
+            <div className="imgContainer">
+               <img src="assets/work.jpg" alt="" />
+            </div>
+         </div>
+
+         <div className="right">
+
+            <div className="sectionOne">
+               <h3>About me</h3>
+               <div className="textContainer">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus nesciunt repudiandae quaerat, amet quod quia consectetur placeat odit error vero nemo sed adipisci alias voluptatum at dolorem doloribus tempora beatae officia ad quidem culpa! Provident recusandae rerum corrupti iste tempore!</p>
+               </div>
+            </div>
+
+            <div className="sectionTwo">
+               <h3>What do i do?</h3>
+               <div className="textContainer">
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus nesciunt repudiandae quaerat, amet quod quia consectetur placeat odit error vero nemo sed adipisci alias voluptatum at dolorem doloribus tempora beatae officia ad quidem culpa! Provident recusandae rerum corrupti iste tempore!</p>
+               </div>
+            </div>
+
+         </div>
+
+
       </div>
    )
 }
